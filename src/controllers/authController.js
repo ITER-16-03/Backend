4885fde7a5c3b55b52efe25701d9c7a4eeee8664
AmitSingh2329 <amitsingh731803@gmail.com>
2026-04-04 12:09:43 +0530

    frontend link updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -62,7 +62,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
